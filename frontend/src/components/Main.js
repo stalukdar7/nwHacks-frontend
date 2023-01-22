def0ffd0695b975e69ed1dedba8082c3fc7b7e7e
Alec Jones <alecjones@hey.com>
2023-01-22 09:30:29 -0800

    added resume page

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import JobPage from './JobPage';
 import SignUp from './SignUp';
+import Resume from './Resume';
 
 const Main = () => {
   return (
     <Routes> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' element={<JobPage />}></Route>
       <Route path='/signup' element={<SignUp />}></Route>
+      <Route path="/resume" element={<Resume />}></Route>
     </Routes>
   );
 }
